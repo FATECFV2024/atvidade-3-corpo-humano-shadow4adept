568ded40ca75b878f9d125bc361460d730798e05
Hirota <93461467+shadow4adept@users.noreply.github.com>
2024-09-26 23:47:54 -0300

    [feat] CorpoHumano.java

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,32 @@
 package main.java;
 
 public class CorpoHumano {
-
+    private float massa;
+    private float volume;
+    private float densidade;
+    public CorpoHumano(float massa, float volume) {
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = calcularDensidade();
+    }
+    public float getMassa() {
+        return massa;
+    }
+    public float getVolume() {
+        return volume;
+    }
+    public float getDensidade() {
+        return densidade;
+    }
+    public void setMassa(float massa) {
+        this.massa = massa;
+        this.densidade = calcularDensidade();
+    }
+    public void setVolume(float volume) {
+        this.volume = volume;
+        this.densidade = calcularDensidade();
+    }
+    private float calcularDensidade() {
+        return massa / volume;
+    }
 }
